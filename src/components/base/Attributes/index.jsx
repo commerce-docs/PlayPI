266e30f73d45ab0144f55ac2eb112420001c5261
Bruce Denham <bdenham@adobe.com>
2023-08-09 19:33:00 -0500

    refactor: break up the code into modules to make it easier to understand

--- src/components/base/Attributes/index.jsx
+++ src/components/base/Attributes/index.jsx
@@ -1,33 +1,47 @@
 export default function Attributes({ attributes, product }) {
   console.log('product:', product);
   console.log('attributes:', attributes);
+
   if (!attributes) return null;
+
   return (
     <div className="mt-5 border-t border-gray-200 pt-1">
       <div className="prose prose-sm mt-4 text-gray-600">
         <ul>
           {attributes.map((attribute) => (
-            <li key={attribute.attribute_metadata.uid}>
-              <span className="text-gray-600 font-light">
-                {attribute.attribute_metadata.label}
-                {': '}
-              </span>
-              {attribute.selected_attribute_options.attribute_option?.length > 1
-                ? attribute.selected_attribute_options.attribute_option.map((option, index) => (
-                    <span key={option.uid} className="text-gray-900 font-normal">
-                      {option.label}
-                      {index !== attribute.selected_attribute_options.attribute_option.length - 1 && ', '}
-                    </span>
-                  ))
-                : attribute.selected_attribute_options.attribute_option?.length > 0 && (
-                    <span className="text-gray-900 font-normal">
-                      {attribute.selected_attribute_options.attribute_option[0].label}
-                    </span>
-                  )}
-            </li>
+            <AttributeItem key={attribute.attribute_metadata.uid} attribute={attribute} />
           ))}
         </ul>
       </div>
     </div>
   );
+}
+
+function AttributeItem({ attribute }) {
+  return (
+    <li>
+      <span className="text-gray-600 font-light">
+        {attribute.attribute_metadata.label}
+        {': '}
+      </span>
+      <AttributeOptions options={attribute.selected_attribute_options.attribute_option} />
+    </li>
+  );
+}
+
+function AttributeOptions({ options }) {
+  if (options?.length > 1) {
+    return options.map((option, index) => (
+      <span key={option.uid} className="text-gray-900 font-normal">
+        {option.label}
+        {index !== options.length - 1 && ', '}
+      </span>
+    ));
+  }
+
+  if (options?.length > 0) {
+    return <span className="text-gray-900 font-normal">{options[0].label}</span>;
+  }
+
+  return null;
 }
