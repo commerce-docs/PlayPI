530f7143e012c2d09c64cecd440d675d2a872033
Bruce Denham <brucedenham@mac.com>
2023-11-04 16:28:01 +0000

    feat: add codesandbox.io files and update endpoint

--- src/api/endpoint.js
+++ src/api/endpoint.js
@@ -1,4 +1,4 @@
 export const endpoint =
   process.env.NODE_ENV === 'production'
-    ? 'https://venia.magento.com/graphql' // <-- for production use in codesandbox.
+    ? 'https://pwa-n3sumvi-7e6vfh6pz64wy.eu-4.magentosite.cloud/graphql' // <-- for production use in codesandbox.
     : 'http://localhost:3001'; // <-- for local development server.js
